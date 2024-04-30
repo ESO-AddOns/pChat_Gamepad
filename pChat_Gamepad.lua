e77fc7c3c2be9e3a3612fff64ff06a03c80a4541
Jinwoo Jeon <jinwoo.jeon0@gmail.com>
2024-04-30 14:54:18 +0900

    Extract hook_last_KEYBOARD_CHAT_SYSTEM_use()

--- pChat_Gamepad.lua
+++ pChat_Gamepad.lua
@@ -2,16 +2,20 @@ local addon = {
     name = 'pChat_Gamepad'
 }
 
-local function OnAddonLoaded(event, name)
-    if name ~= pChat.CONSTANTS.ADDON_NAME then return end
-
-    EVENT_MANAGER:UnregisterForEvent(addon.name, EVENT_ADD_ON_LOADED, OnAddonLoaded)
-
+local function hook_last_KEYBOARD_CHAT_SYSTEM_use()
     local original_pChat_ApplyChatConfig = pChat.ApplyChatConfig
     pChat.ApplyChatConfig = function()
         original_pChat_ApplyChatConfig()
         KEYBOARD_CHAT_SYSTEM:RefreshVisibility()
     end
+end
+
+local function OnAddonLoaded(event, name)
+    if name ~= pChat.CONSTANTS.ADDON_NAME then return end
+
+    EVENT_MANAGER:UnregisterForEvent(addon.name, EVENT_ADD_ON_LOADED, OnAddonLoaded)
+
+    hook_last_KEYBOARD_CHAT_SYSTEM_use()
 end
 
 EVENT_MANAGER:RegisterForEvent(addon.name, EVENT_ADD_ON_LOADED, OnAddonLoaded)
