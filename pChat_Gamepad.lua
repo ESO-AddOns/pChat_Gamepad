54bfbee335a7a36558bb4f2f06e049fcae109f79
Jinwoo Jeon <jinwoo.jeon0@gmail.com>
2024-05-19 13:57:29 +0900

    Display message for deprecating

--- pChat_Gamepad.lua
+++ pChat_Gamepad.lua
@@ -15,7 +15,20 @@ local function OnAddonLoaded(event, name)
 
     EVENT_MANAGER:UnregisterForEvent(addon.name, EVENT_ADD_ON_LOADED, OnAddonLoaded)
 
-    hook_last_KEYBOARD_CHAT_SYSTEM_use()
+    local messageParams = CENTER_SCREEN_ANNOUNCE:CreateMessageParams(CSA_CATEGORY_SMALL_TEXT, SOUNDS.GENERAL_ALERT_ERROR)
+    messageParams:SetCSAType(CENTER_SCREEN_ANNOUNCE_TYPE_DISPLAY_ANNOUNCEMENT)
+    messageParams:SetExpiringCallback(function ()
+        CENTER_SCREEN_ANNOUNCE:ReleaseMessageParams(messageParams)
+    end)
+    CENTER_SCREEN_ANNOUNCE:AddMessageWithParams(messageParams)
+
+    if pChat.CONSTANTS.ADDON_VERSION < '10.0.5.2' then
+        messageParams:SetText('|cda8a00pChat_Gamepad|r has been deprecated. Update |cda8a00pChat|r.')
+
+        hook_last_KEYBOARD_CHAT_SYSTEM_use()
+    else
+        messageParams:SetText('|cda8a00pChat_Gamepad|r has been deprecated. Delete |cda8a00pChat_Gamepad|r.')
+    end
 end
 
 EVENT_MANAGER:RegisterForEvent(addon.name, EVENT_ADD_ON_LOADED, OnAddonLoaded)
